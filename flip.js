da09d423645389c242421462edc7fab587f36309
la1perry <lperry1884@gmail.com>
2018-01-30 11:47:26 -0800

    pages,error-message

--- flip.js
+++ flip.js
@@ -1,57 +1,57 @@
-$(function(){
-$.support.css3d=supportsCSS3D();
+// $(function(){
+// $.support.css3d=supportsCSS3D();
 
-var container=$('#flip-container');
+// var container=$('#flip-container');
 
-$('#toRecover').on('click',function(e){
-    flipit($('#recover'), e);
-    $('#signup').addClass("neat");
-    $('#recover').removeClass("neat",3000); 
-});
+// $('#toRecover').on('click',function(e){
+//     flipit($('#recover'), e);
+//     $('#signup').addClass("neat");
+//     $('#recover').removeClass("neat",3000); 
+// });
 
-$('#join').on('click', function(e){
-    flipit($('#signup'),e);
-    $('#recover').addClass("neat");
-    $('#signup').removeClass("neat",3000);   
-});
+// $('#join').on('click', function(e){
+//     flipit($('#signup'),e);
+//     $('#recover').addClass("neat");
+//     $('#signup').removeClass("neat",3000);   
+// });
 
-$('#jcancel').on('click',function(e){
-    flipit($('#signup'),e);
-});
-$('#cancelit').on('click',function(e){
-    flipit($('#signup'),e);  
-});
+// $('#jcancel').on('click',function(e){
+//     flipit($('#signup'),e);
+// });
+// $('#cancelit').on('click',function(e){
+//     flipit($('#signup'),e);  
+// });
 
-// disable after backend added
-formContainer.find('form').submit(function(e){
-    e.preventDefault();
-});
-// to here
+// // disable after backend added
+// formContainer.find('form').submit(function(e){
+//     e.preventDefault();
+// });
+// // to here
 
-function ctoggle(){
-    return container.toggleClass('flipped').delay(200);
-}
+// function ctoggle(){
+//     return container.toggleClass('flipped').delay(200);
+// }
 
-function flipit(el,e){
-$.when(ctoggle()).done(function(){
-    el.toggleClass('hide');
-    if(!$.support.css3d)
-    $('#signin').toggle();
+// function flipit(el,e){
+// $.when(ctoggle()).done(function(){
+//     el.toggleClass('hide');
+//     if(!$.support.css3d)
+//     $('#signin').toggle();
 
-});
-e.preventDefault();
-}
+// });
+// e.preventDefault();
+// }
 
-function supportsCSS3D(){
-    var props=[
-        'perspectiveProperty','WebkitPerspective','MozPerspective'
-    ], testDom=document.createElement('a');
-    for(var i=0;i<props.length;i++){
-        if(props[i] in testDom.style){
-            return true;
-        }
-    }
-    return false;
-}
+// function supportsCSS3D(){
+//     var props=[
+//         'perspectiveProperty','WebkitPerspective','MozPerspective'
+//     ], testDom=document.createElement('a');
+//     for(var i=0;i<props.length;i++){
+//         if(props[i] in testDom.style){
+//             return true;
+//         }
+//     }
+//     return false;
+// }
 
-});
+// });
